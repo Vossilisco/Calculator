4119ea1dba59a208769a5edba1c000ca8086df01
Rafael M <un_segundo@hotmail.com>
2017-05-05 17:40:42 +0200

    Added parenthesis support
    Added full parenthesis support, input cheking for security and
    exceptions handling.

--- src/myPackage/Calculator.java
+++ src/myPackage/Calculator.java
@@ -1,34 +1,47 @@
 package myPackage;
 
 import java.util.ArrayList;
+import java.util.EmptyStackException;
 import java.util.Scanner;
 import java.util.Stack;
 
-
 public class Calculator {
 
-	private final String operators = "+-*/^";
+	private final String operators = "+-*/^()";
 	private ArrayList<Character> operatorList;
 	private ArrayList<Double> operandList;
-	
+
+	/**
+	 * Default constructor
+	 * */
 	public Calculator(){
 		this.operatorList = new ArrayList<Character>();
 		this.operandList = new ArrayList<Double>();
 		welcome();
 	}
-	
+
 	/**
-	 * Exactly the as System.out.println but shorter
+	 * Exactly the same as System.out.println but shorter
 	 * */
 	private void println(String s){
 		System.out.println(s);
 	}
-		
+
 	/**
-	 * Exactly the as System.out.print but shorter
+	 * Exactly the same as System.out.print but shorter
 	 * */
 	private void print(String s){
 		System.out.print(s);
+	}
+
+	/**
+	 * Print the given error message and force the end of the program.
+	 * @param errorMessage String: Containts the error message that will be printed.
+	 * 
+	 * */
+	private void exitBecauseError(String errorMessage){
+		System.err.println("ERROR -> " + errorMessage);
+		System.exit(1);
 	}
 	
 	/**
@@ -44,30 +57,30 @@ public class Calculator {
 		println("Ready to start:");
 		println("\n");
 	}
-	
+
 	/**
 	 * Return true if the input String is a valid operator.
 	 * 
 	 **/
 	private boolean isOperator(String o){
-		
+
 		return operators.contains(o);
 	}
-		
+
 	/**
 	 * Return true if the input String is a number.
 	 * 
 	 **/
 	private boolean isNumeric(String strin)  {  
-		  try  {  
-			  double d = Double.parseDouble(strin); 
-			  
-		  } catch(NumberFormatException nfe){  
-			  return false;  
-		  }  
-		  return true;  
-		}
-	
+		try  {  
+			double d = Double.parseDouble(strin); 
+
+		} catch(NumberFormatException nfe){  
+			return false;  
+		}  
+		return true;  
+	}
+
 	/**
 	 * Return true if the input char is a number.
 	 * 
@@ -75,7 +88,7 @@ public class Calculator {
 	private boolean isNumeric(char strin)  {  
 		return Character.isDigit(strin) || strin=='.'; 
 	}
-	
+
 	/**
 	 * Return the priority of the given operator.
 	 * @param op Char with the valid operator.
@@ -93,6 +106,9 @@ public class Calculator {
 
 		case '^':
 			return 3;
+		case '(':
+		case ')':
+			return 0;
 
 		default:
 			//It will never reach this
@@ -102,9 +118,9 @@ public class Calculator {
 
 	/**
 	 * It calculates the mathematical operation with the input parameters
-	 * @param op Char that means a valid operation
-	 * @param value1 First operand
-	 * @param value2 Second operand
+	 * @param op char: A valid operation
+	 * @param value1 double: First operand
+	 * @param value2  double: Second operand
 	 * */
 	private double calculate(char op, double value1, double value2){
 		switch (op) {
@@ -113,24 +129,24 @@ public class Calculator {
 
 		case '-':
 			return value2-value1;
-			
+
 		case '*':
 			return value2*value1;
-			
+
 		case '/':
 			return value2/value1;
-		
+
 		case '^':
 			return Math.pow(value2,value1);
-			
+
 		default:
 			return 0;
 		}
 	}
-	
+
 	/**
 	 * Convert a String math expression in infix to Postfix form.
-	 * @param String with the infix expression
+	 * @param infix String with the infix expression
 	 * @return String with the postfix expression
 	 **/
 	public String infixToPostfix(String infix) {
@@ -139,28 +155,75 @@ public class Calculator {
 		StringBuilder temp = new StringBuilder();
 
 		for (int i = 0; i < infix.length(); i++) {
-			// If char is an operand or one digit of a double number then add to output
+			//Reset auxiliary variable
 			temp.delete(0, temp.length());
-			
+
+			//Fill with the full operand/operator
 			while(infix.charAt(i)!= ' '){
 				temp.append(infix.charAt(i));
 				i++;
 			}
-			
-			if (isNumeric(temp.toString())) {			
+
+			//If char is an operand add to output
+			if (isNumeric(temp.toString())) {		
 				postFix.append(temp.toString());
 				postFix.append(" ");
-				
-			} else {
+
+				//If it is an operator
+			}else{
+
 				char operator = temp.charAt(0);
-				while (!stack.isEmpty() && getPriority(operator) <= getPriority(stack.peek())) {
-					postFix.append(stack.pop());
-					postFix.append(" ");
+
+				//If it is a left parenthesis we add it and it will mark like a set point in the expression because mathematical priorities reasons
+				if (operator == '('){	
+
+					stack.push(operator);
+
+					//Before adding it to the stack, it has to be sorted and respecting the priorities letting the 
+				}else if(getPriority(operator) > 0){
+
+					//We output all the operators who has more or the same priority that found
+					while (!stack.isEmpty() && getPriority(operator) <= getPriority(stack.peek())) {
+
+						postFix.append(stack.pop());
+						postFix.append(" ");
+
+					}
+					//When others operators are out, we save our with less priority
+					stack.push(operator);
+
+					//If it is an right parenthesis we must output all the operators found since the left parenthesis
+				}else if (operator == ')'){
+
+					if(stack.empty()){
+
+						exitBecauseError("Problem with parenthesis, bad sintaxis.");
+
+						//If the first operator is the left parenthesis it just deletes it
+					}else if(stack.peek()=='('){
+
+						stack.pop();
+
+					}else{
+						//If not, it just output the operators until find the left parenthesis
+						while(!stack.empty() && stack.peek()!='('){
+							postFix.append(stack.pop());
+							postFix.append(" ");
+						}
+						if(stack.empty()){
+
+							exitBecauseError("Problem with parenthesis, bad sintaxis.");
+
+							//Left parenthesis found so now, it deletes it.
+						}else if(stack.peek().equals('(')){
+
+							stack.pop();
+
+						}
+					}
 				}
-				stack.push(operator);
 			}
 		}
-
 		while (!stack.isEmpty()) {
 			postFix.append(stack.pop());
 			postFix.append(" ");
@@ -170,64 +233,120 @@ public class Calculator {
 	}
 
 	/**
-	 * Ask the user for the math expresion on infix form.
+	 * Check if the input String is using parenthesis properly or not.
+	 * @param str String with the mathematical expression to check
+	 * @return true if it is right, false if there is something wrong
+	 **/
+	private boolean parenthesisCheck(String str)  {
+		Stack<Character> stack = new Stack<Character>();
+		char c;
+
+		for(int i=0; i < str.length(); i++) {
+			c = str.charAt(i);
+
+			if(c == '(')
+				stack.push(c);
+			else if(c == ')')
+				if(stack.empty())
+					return false;
+				else if(stack.peek() == '(')
+					stack.pop();
+				else
+					return false;
+		}
+		return stack.empty();
+	}
+
+	/**
+	 * Ask the user for the math expression on infix form.
 	 * @return String that contains a infix expression
 	 * */
 	public String askForExpression(){
 		StringBuilder sb = new StringBuilder();
 		Scanner reader = new Scanner(System.in);
 		String input = "";
-		
+
 		while(!input.equals("e")){
 			input = reader.nextLine();
-			
-			if(isOperator(input) && operatorList.size()+1==operandList.size()){
+
+
+			if(isOperator(input)){
+
 				sb.append(input);
-				operatorList.add(input.charAt(0));
+				
+				if(!input.equals("(") && !input.equals(")")) {
+					
+					try{
+						operatorList.add(input.charAt(0));
+					}catch(StringIndexOutOfBoundsException s){
+						
+						exitBecauseError("No input. Please type only numbers and valid operators: +-*/^() ");
+					}
+				}
 				sb.append(" ");
-			}else if(isNumeric(input) && operatorList.size()==operandList.size()){
+
+			}else if(isNumeric(input)){
+
 				sb.append(input);
+
 				operandList.add(Double.parseDouble(input));
-				sb.append(" ");
+
+				sb.append(" ");	
+
 			}else if (!input.equals("e")){
-				print("ERROR: input should enter number, operator and number. Introduce 'e' when finish.");
-				System.exit(0);
+
+				exitBecauseError("Unknown input. Please type only numbers and valid operators: +-*/^() ");
 			}
 		}
 		reader.close();
-		return sb.toString();
+
+		if(!parenthesisCheck(sb.toString())){
+			exitBecauseError("Invalid use of parenthesis.");
+			return "";
+			
+		}else if (operandList.size() > operatorList.size()+1){
+			exitBecauseError("Invalid sintaxis. The expression: " + sb.toString() + " has no mathematical sense.");
+			return "";
+		}else{
+			return sb.toString();
+		}
+
 	}
-	
 
 	/**
 	 * It calculates the result of a PostFix given expression.
 	 * @param postfix String with the mathematical expression in PostFix form.
-	 * @return Double data with the solution.
+	 * @return double data with the solution.
 	 * */
 	public double evaluatePostfix(String postfix){
 		Stack<Double> pila = new Stack<Double>();
 		StringBuilder temp = new StringBuilder();
-		
+
 		for (int i=0; i< postfix.length(); i++){
 			temp.delete(0, temp.length());
-			
+
 			while(postfix.charAt(i)!= ' '){
 				temp.append(postfix.charAt(i));
 				i++;
 			}
-			
+
 			//If is a number, we push it into the stack
 			if(isNumeric(temp.toString())){
 				pila.push(Double.valueOf(temp.toString()));
-		
-			//If is an operator, we operate with it
+
+				//If is an operator, we operate with it
 			}else{
-				
-				pila.push(calculate(temp.charAt(0), pila.pop(), pila.pop()));
-				
+
+				try{
+					pila.push(calculate(temp.charAt(0), pila.pop(), pila.pop()));
+
+				}catch(EmptyStackException e){
+
+					exitBecauseError("Problem with operators.");
+					System.exit(1);
+				}
 			}
 		}
-		
 		return pila.pop();
 	}
 
--- src/myPackage/UsingCalculator.java
+++ src/myPackage/UsingCalculator.java
@@ -13,14 +13,12 @@ public class UsingCalculator {
 	public static void main(String[] args) {
 		
 		String infix = calculadora.askForExpression();
-		System.out.println("Infix form: " + infix);
-		
 		String postfix = calculadora.infixToPostfix(infix);
-		System.out.println("Postfix form: " + postfix);
-		
 		Double solution = calculadora.evaluatePostfix(postfix);
+		
+		System.out.println("Infix form: " + infix);
+		System.out.println("Postfix form: " + postfix);
 		System.out.println("The solution is: " + solution.toString());
 		
 	}
-
 }
